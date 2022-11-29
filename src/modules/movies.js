ecc6d8d71a1d565b296bad2b212182bbd73641b9
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-11-29 16:23:47 +0200

    Add main part of the page and popullate the content from API

---
+++ src/modules/movies.js
@@ -0,0 +1,16 @@
+export default class Movies {
+    BASE_URL='https://jsonplaceholder.typicode.com/todos/1';
+    constructor(){
+
+    }
+
+    static displayMovie= async() =>{
+        const response = await fetch('https://api.tvmaze.com/seasons/1/episodesn');
+        console.log(response);
+        const movies = await response.json();
+        console.log("Movies:", JSON.stringify(movies))
+        return movies;
+        // const title="The Walking Dead";
+        // return title;
+    }
+}
