58ed98603b006bdc500708c0227f95de8fe5211d
krisbobo <kiyongbohbo@gmail.com>
2022-11-30 20:32:10 +0100

    Add function which displays popup

--- src/modules/popullateMovie.js
+++ src/modules/popullateMovie.js
@@ -1,7 +1,8 @@
 import * as el from './domElements.js';
+import { commentBtn } from './domElements.js';
 import Movies from './movies.js';
 
-export const showMovies=  () => { 
+export const showMovies =  () => { 
     Movies.displayMovie().then((res) => {
         res.forEach(res => {
             el.cardContainer.innerHTML += ` <div class="card">
@@ -14,10 +15,38 @@ export const showMovies=  () => {
                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                     </svg>5 likes</span>
                 </div>
-                <button class="btn">Comments</button>
+                <button onclick="popUp()" class="btn" id="comment-btn">Comments</button>
                 <button class="btn">Reservations</button>
                 </div>`;
         })
-    })
-
+    });
+    window.popUp = () => {
+    Movies.displayMovie().then((res) => {
+        res.forEach(res => {
+            const pop = document.querySelector('.pop');
+            pop.innerHTML = `
+            <div class="popup">
+                <div class="content">
+                    <div class="popup-body">
+                        <img src="${res.image.original}" alt="Current movie">
+                        <span class="close">&times;</span>                   
+                    </div>
+                    <div class="movie-type">
+                        <p class="name">${res.name}</p>
+                        <p class="runtime">Duration: ${res.runtime} mins</p>
+                    </div>
+                    <div class="movie-details">
+                        <p class="summary">${res.summary}</p>
+                        <p class="airdate">Airing date: ${res.airdate}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+            const closeBtn = document.querySelector('.close');
+            closeBtn.addEventListener('click', () => {
+                window.location.reload();
+            });
+            })
+        });
+    }
 }
