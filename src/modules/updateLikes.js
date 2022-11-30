44b5ac2d250d1c3168657dab950db7edf005836a
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-11-30 22:26:12 +0200

    Add feature to update likes in the Involvement API and update styling

--- src/modules/updateLikes.js
+++ src/modules/updateLikes.js
@@ -1,15 +1,10 @@
-import * as el from './domElements.js';
 import Involve from './involvementApi.js';
 
-export const updateLikes = async (name,e) => { 
-  const like = await Involve.postLike(name);
+export const updateLikes = async (e) => { 
+  await Involve.postLike(e.id);
   if(e.children[1].nodeName==='B'){
     let count= e.children[1].innerHTML;
     count.trim();
     e.children[1].innerHTML=parseInt(count,10)+1;
-  }
- 
-
-  
-  
+  }  
 }
