ce6ae8202122d903f331d67d7d7e101aaa7ce736
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-01 19:14:38 +0200

    Add test case for all displayed items

--- src/modules/popullateMovie.js
+++ src/modules/popullateMovie.js
@@ -29,7 +29,8 @@ Movies.displayMovie()
         <button class="btn like">Comments</button>
         <button class="btn">Reservations</button></di>
       </div>`;
-      count();
+      console.log( count(el.cardContainer,el.count));
+     
 
         document.querySelectorAll('.like').forEach((like) => {
             like.addEventListener('click', async (e) => {
@@ -39,9 +40,9 @@ Movies.displayMovie()
             });
           });
           
-        document.querySelectorAll('.card').forEach((card) => {
-            card.addEventListener('load',  count);
-          });
+        // document.querySelectorAll('.card').forEach((card) => {
+        //     card.addEventListener('load',  count);
+        //   });
     });    
   })
 }
