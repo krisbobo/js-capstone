ecbb968bb76697cafb19cc3a19223460c653f275
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-01 20:41:16 +0200

    Add TV Show section

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -2,3 +2,5 @@ export const cardContainer = document.querySelector('.card-container');
 export const cards = document.querySelectorAll('.card');
 export const likes = document.querySelectorAll('.like');
 export const count = document.querySelector('.count');
+export const tvShows = document.querySelector('.TV-shows');
+export const tvCardContainer = document.querySelector('.tv-card-container');
