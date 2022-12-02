cf589cbe83611dcf720bde2237b61ee4e131b760
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-02 21:31:57 +0200

    Add create comment feature and update the code for readability

--- src/modules/popullateMovie.js
+++ src/modules/popullateMovie.js
@@ -10,10 +10,9 @@ const cDay = currentDate.getDate();
 const cMonth = currentDate.getMonth() + 1;
 const cYear = currentDate.getFullYear();
 
-export const showMovies= async () => { 
+export const showMovies=  () => { 
 //This displays all movies from the movie API
 Movies.displayMovie()
-
   .then((res) => {
     res.forEach(async (res) =>  {
       const name = res.name;
@@ -38,23 +37,25 @@ Movies.displayMovie()
       </div>`;
       count(el.cardContainer,el.count);
       
-    document.querySelectorAll('.like').forEach((like) => {
-        like.addEventListener('click', async (e) => {
-            // call update function
-
-         const i =await updateLikes(e.target.parentNode);
-        
-         if(i!==0){
-          like.children[1].innerHTML= parseInt(i,10) +1;
-         }
-        });
+      document.querySelectorAll('.like').forEach((like) => {
+      like.addEventListener('click', async (e) => {
+      // call update function
+      const i =await updateLikes(e.target.parentNode);       
+      if(i!==0){
+        like.children[1].innerHTML= parseInt(i,10) +1;
+      }
       });
+    });
         
     document.querySelectorAll('.card').forEach((card) => {
       card.addEventListener('load',  count);
     });
-});    
-})
+  })
+  
+  })
+  .catch ((err) => {
+    console.log(err)    
+  })
 }
 
 window.popUp = async (id,name) => {
@@ -65,7 +66,6 @@ const comments = await Involve.getComments(name);
         commentList += `<div> <p>${com.creation_date}<span>${cDay}/ ${cMonth}/ ${cYear}</span></p>
         <p>${com.username}:</p>
         <p>${com.comment}</p></div>`; 
-        console.log(commentList)
         return commentList;
     });
 }
@@ -74,47 +74,71 @@ commentList = commentList ? commentList : "Be the first to comment";
 pop.classList.add('visible'); 
 
 
-await Movies.displayMovie()
+Movies.displayMovie()
 .then((res) => {
-    res.forEach(res => {
-        if(res.id === id){
-    
-        pop.innerHTML = `<div class="content">
-        <div class="popup-body">
-        <h2 class="name">${res.name}</h2>
-        <span class="close">&times;</span>
-        </div>
-        <div class="poster">
-        <img src="${res.image.original}" alt="${res.name}">
-        </div>
-        <div class="movie-type">
-            ${res.summary}
-        </div>
-        <div class="movie-details">
-            <p class="runtime">Duration: ${res.runtime} mins</p>
-            <p class="airdate">Airing date: ${res.airdate}</p>
-        </div>
-        <div class="comments-container">  ${commentList} </div>
-        <div class="container2">
-        <h2 class="form-title">Add a comment</h2>
-        <form id="form">
-            <input type="text" id="username" placeholder="Your name" required>
-            <textarea type="text" id="text" col="30" placeholder="Your insights" required></textarea>
-            <button id=${id} class="form-btn">Comment</button>
-        </form>
-        </div>`;
-        
-        //Call PostComment on form submission 
-        // postComment(id)
-        console.log( "Post Comment ",  Involve.postComment(name, "chris", "awesome film"));
-        
-        const closeBtn = document.querySelector('.close');
-        closeBtn.addEventListener('click', () => {
-            window.location.reload();
-            pop.classList.remove('visible');
-        });
-        }
-        
+  res.forEach(res => {
+      if(res.id === id){
+  
+      pop.innerHTML = `<div class="content">
+      <div class="popup-body">
+      <h2 class="name">${res.name}</h2>
+      <span class="close">&times;</span>
+      </div>
+      <div class="poster">
+      <img src="${res.image.original}" alt="${res.name}">
+      </div>
+      <div class="movie-type">
+          ${res.summary}
+      </div>
+      <div class="movie-details">
+          <p class="runtime">Duration: ${res.runtime} mins</p>
+          <p class="airdate">Airing date: ${res.airdate}</p>
+      </div>
+      <div class="comments-container">  ${commentList} </div>
+      <div class="container2">
+      <h2 class="form-title">Add a comment</h2>
+      <form id="form">
+          <input type="text" id="username" placeholder="Your name" required>
+          <textarea type="text" id="comment" col="30" placeholder="Your insights" required></textarea>
+      
+          <button type='submit' id=${res.name} class="form-btn">Comment</button>
+      </form>
+      </div>`;
+      
+      document.querySelectorAll('.form-btn').forEach((formBtn) => {
+        formBtn.addEventListener('click', async (e) =>{
+          e.preventDefault;
+          const id =formBtn.id;
+          console.log(id)
+          const parent = formBtn.closest('form');
+          const name = parent.children[0].value;
+          const feedback = parent.children[1].value;
+          const small = parent.children[1];
+          if(name.trim()!=="" && feedback.trim()!==""){
+            Involve.postComment(id, name, feedback);
+            small.innerHTML="Thanks for your comment";
+            form.reset();
+          }
+          else{
+            small.innerHTML="Please fill out the form, name and comment cannot be empty!"
+          }
         })
-    });
+      })
+
+      //Call PostComment on form submission 
+      // postComment(id)
+      //console.log( "Post Comment ",  Involve.postComment(name, "chris", "awesome film"));
+      
+      const closeBtn = document.querySelector('.close');
+      closeBtn.addEventListener('click', () => {
+          window.location.reload();
+          pop.classList.remove('visible');
+      });
+      }
+      
+      })
+  })
+  .catch((err) =>{
+    console.log(err)
+  })
 }
